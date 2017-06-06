7ccc814953457f4c039d33610b5ea012a314b9d5
Aarni Koskela <akx@iki.fi>
2017-06-06 11:00:35 +0300

    Initial commit

---
+++ {{cookiecutter.project_name}}/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "max-len": ["error", 120],
+    "object-curly-spacing": "off",
+    "no-mixed-operators": "off",
+    "arrow-parens": "off",
+    "comma-dangle": [
+      "error", {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        "functions": "ignore",
+      }
+    ],
+  },
+};
